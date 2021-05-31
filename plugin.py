377782251511b05d7242813db091b7098517662e
MaliciaMrg <maliciamrg@users.noreply.github.com>
2021-05-31 21:56:04 +0200

    adaptation vsx 528 display

--- plugin.py
+++ plugin.py
@@ -127,6 +127,8 @@ class BasePlugin:
                 self.power_on = False
         elif loaction == 'VOL': #VOLUME
             self.volume_level = str(round((int(str(lodetail))*100)/161))
+            Domoticz.Debug('self.volume_level:'+str(self.volume_level))
+            Domoticz.Debug('lodetail:'+str(lodetail))
         elif loaction == 'MUT': #MUTE
             if lodetail == '0': #MUT0
                 self.mute = 'On'
@@ -181,8 +183,10 @@ class BasePlugin:
                 self.PioneerConn.Send(Message='MF\r', Delay=0)
             elif (action == "Set"):
             #de 0-80% de 000 a 161
-                level = ((int(str(Level))*161)/80)  #Level en %, 80% ou plus => max soit 161
+                level = str(round((int(str(lodetail))*161)/80))  #Level en %, 80% ou plus => max soit 161
                 delta = level - int(str(self.volume_level))
+                Domoticz.Debug('lodetail:'+str(lodetail))
+                Domoticz.Debug('level:'+str(level))
                 Domoticz.Debug("delta: "+str(delta))
                 for i in range(0, abs(delta), self.VOLUMESTEP):
                     if delta < 0:
@@ -195,6 +199,7 @@ class BasePlugin:
                         Domoticz.Debug("VU")
 
                 self.volume_level = str(round(int(level))).rjust(3,'0')
+                Domoticz.Debug('self.volume_level:'+str(self.volume_level))
 #                 self.PioneerConn.Send(Message=self.volume_level+'VL\r', Delay=0)
 
             elif (action == "Off"):
